8e23d9b43fb448f71d094c35e534569bcf221084
cobra-dz <docteryounes04@gmail.com>
2026-07-13 19:12:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+window.addEventListener("scroll", function () {
+    const nav = document.querySelector(".navbar");
 
+    if (window.scrollY > 50) {
+        nav.style.background = "#050b15";
+        nav.style.boxShadow = "0 10px 25px rgba(0,0,0,.4)";
+    } else {
+        nav.style.background = "rgba(5,10,25,.95)";
+        nav.style.boxShadow = "none";
+    }
+});
+
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity="1";
+entry.target.style.transform="translateY(0)";
+
+}
+
+});
+
+});
+
+cards.forEach(card=>{
+
+card.style.opacity="0";
+
+card.style.transform="translateY(60px)";
+
+card.style.transition=".8s";
+
+observer.observe(card);
+
+});
